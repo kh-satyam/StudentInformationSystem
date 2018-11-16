b7cdb1cb7d2afeb3c32817f40f98306e0a485dcf
kh-satyam <kh.satyam09@gmail.com>
2018-11-17 02:38:25 +0530

    added carousal

--- StudentInformationSystem/WebContent/js/displayRange.js
+++ StudentInformationSystem/WebContent/js/displayRange.js
@@ -21,14 +21,22 @@ function fetchResults(){
 	        console.log(data);
 	        var populate=document.getElementById('display');
 	        var html='<div id="myCarousel" class="carousel slide" data-ride="carousel">';
-	        html+='<ol class="carousel-indicators">;';
+	        html+='<ol class="carousel-indicators">';
 	        var i;
 	        var l1;
 	        for(i=0;i<data.length;i++){
-	        	l1='<li data-target="#myCarousel" data-slide-to="';
-	        	l1+=i;
-	        	l1+='" class="active"></li>';
-	        	html+=l1;
+	        	if(i==0){
+	        		l1='<li data-target="#myCarousel" data-slide-to="';
+		        	l1+=i;
+		        	l1+='" class="active"></li>';
+		        	html+=l1;
+	        	}else{
+	        		l1='<li data-target="#myCarousel" data-slide-to="';
+		        	l1+=i;
+		        	l1+='"></li>';
+		        	html+=l1;
+	        	}
+	        	
 	        }
 	        html+='</ol>';
 	        html+='<div class="carousel-inner">';
@@ -36,14 +44,40 @@ function fetchResults(){
 	        for(i=0;i<data.length;i++){
 	        	if(i==0){
 	        		l1='<div class="item active">';
-	        		
+	        		l1+='<img src=';
+	        		var url='"http://localhost:8080/restServices/webapi/student/download/image/'+data[i]['rollNumber'];
+	        		l1+=url;
+	        		l1+= '" alt="Los Angeles">';
+	        		l1+='<div class="carousel-caption">';
+	        		l1+='<h3>'+data[i]['name']+'</h3><p>'+data[i]['totalMarks']+'</p></div>';
+	        		l1+='</div>';
+	        		html+=l1;
 	        	}else{
-	        		
+	        		l1='<div class="item">';
+	        		l1+='<img src="';
+	        		var url='"http://localhost:8080/restServices/webapi/student/download/image/'+data[i]['rollNumber'];
+	        		l1+=url;
+	        		l1+= ' "alt="Los Angeles">';
+	        		l1+='<div class="carousel-caption">';
+	        		l1+='<h3>'+data[i]['name']+'</h3><p>'+data[i]['totalMarks']+'</p></div>';
+	        		l1+='</div>';
+	        		html+=l1;
 	        	}
 	        }
 	        
 	        html+='</div>';
+	        
+	        html+='<a class="left carousel-control" href="#myCarousel" data-slide="prev">';
+	        html+='<span class="glyphicon glyphicon-chevron-left"></span>';
+	        html+='<span class="sr-only">Previous</span>';
+	        html+='</a>';
+	        html+='<a class="right carousel-control" href="#myCarousel" data-slide="next">';
+	        html+='<span class="glyphicon glyphicon-chevron-right"></span>';
+	        html+='<span class="sr-only">Next</span>';
+	        html+='</a>';
+	        html+='</div>';
 	        console.log(html);
+	        populate.innerHTML=html;
 	    }
 	};
 }
