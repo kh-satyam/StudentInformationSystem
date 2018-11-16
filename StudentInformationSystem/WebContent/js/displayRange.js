b915b49548cd0cc4bc9541e8217b87b1cd3b30b1
kh-satyam <kh.satyam09@gmail.com>
2018-11-17 03:14:04 +0530

    added carousal

--- StudentInformationSystem/WebContent/js/displayRange.js
+++ StudentInformationSystem/WebContent/js/displayRange.js
@@ -20,7 +20,7 @@ function fetchResults(){
 	        var data = JSON.parse(xhr.responseText);
 	        console.log(data);
 	        var populate=document.getElementById('display');
-	        var html='<div id="myCarousel" class="carousel slide" data-ride="carousel">';
+	        var html='<div id="myCarousel" style="width:400px;height:400px;" class="carousel slide" data-ride="carousel">';
 	        html+='<ol class="carousel-indicators">';
 	        var i;
 	        var l1;
@@ -39,27 +39,33 @@ function fetchResults(){
 	        	
 	        }
 	        html+='</ol>';
-	        html+='<div class="carousel-inner">';
+	        html+='<div  style="width:100%;height:100%;" class="carousel-inner">';
 	        
 	        for(i=0;i<data.length;i++){
 	        	if(i==0){
-	        		l1='<div class="item active">';
-	        		l1+='<img src=';
+	        		l1='<div  style="width:100%;height:100%;"class="item active">';
+	        		l1+='<img'+' style="width:100%;height:100%;" src=';
 	        		var url='"http://localhost:8080/restServices/webapi/student/download/image/'+data[i]['rollNumber'];
 	        		l1+=url;
-	        		l1+= '" alt="Los Angeles">';
+	        		l1+= ' "alt="';
+	        		l1+=data[i]['rollNumber'];
+	        		l1+='">';
 	        		l1+='<div class="carousel-caption">';
-	        		l1+='<h3>'+data[i]['name']+'</h3><p>'+data[i]['totalMarks']+'</p></div>';
+	        		l1+='<h3>'+data[i]['name']+'</h3><h4>'+data[i]['rollNumber']+'</h4>';
+	        		l1+='<h3>'+data[i]['totalMarks']+'</h4><p>'+data[i]['grade']+'</h4></div>';
 	        		l1+='</div>';
 	        		html+=l1;
 	        	}else{
-	        		l1='<div class="item">';
-	        		l1+='<img src="';
+	        		l1='<div style="width:100%;height:100%;" class="item">';
+	        		l1+='<img'+' style="width:100%;height:100%;" src=';
 	        		var url='"http://localhost:8080/restServices/webapi/student/download/image/'+data[i]['rollNumber'];
 	        		l1+=url;
-	        		l1+= ' "alt="Los Angeles">';
+	        		l1+= ' "alt="';
+	        		l1+=data[i]['rollNumber'];
+	        		l1+='">';
 	        		l1+='<div class="carousel-caption">';
-	        		l1+='<h3>'+data[i]['name']+'</h3><p>'+data[i]['totalMarks']+'</p></div>';
+	        		l1+='<h3>'+data[i]['name']+'</h3><h4>'+data[i]['rollNumber']+'</h4>';
+	        		l1+='<h3>'+data[i]['totalMarks']+'</h4><p>'+data[i]['grade']+'</h4></div>';
 	        		l1+='</div>';
 	        		html+=l1;
 	        	}
