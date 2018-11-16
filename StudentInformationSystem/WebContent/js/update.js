edc80b429b2269bbe2e9e7a2314c4d13dbd7751a
Jagmeet Singh <jagmeet787@gmail.com>
2018-11-17 03:16:08 +0530

    Form validations

--- StudentInformationSystem/WebContent/js/update.js
+++ StudentInformationSystem/WebContent/js/update.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
-	
+	$('.msg').hide();
 	var sbutton=document.getElementById('updateStudent');
 	var toggler=document.getElementById('toggle');
 	var z = document.getElementById("t2");
@@ -29,20 +29,45 @@ $(document).ready(function() {
 	}
 
 	function updateRecord(){
+		$('.msg').hide();
 		var x = document.forms.namedItem("form3");
 		var i;
 		var arr = {};
 		var form = new FormData();
+		// validating roll number
 		for(i=0;i<x.length;i++){
-				arr[x.elements[i].name]=x.elements[i].value;
+			if ( x.elements[i].name == "rollNumber" ) {
+				var rollNumber = parseInt(x.elements[i].value);
+				if ( isNaN(rollNumber) || rollNumber < 1) {$("#rollNumber-msg").show("slow"); return;}
+			}
+			arr[x.elements[i].name]=x.elements[i].value;
 		}
 		console.log(arr);
 		form.append("json",arr);
 		var val1=arr["rollNumber"];
 		var val2=arr["parameter"];
 		var val3=arr["value1"];
+		
+		// validating marks 
+		if ( arr["parameter"] == "physicsMarks" || arr["parameter"] == "chemistryMarks" 
+		|| arr["parameter"] == "mathematicsMarks"  ) {
+			var marks = arr["value1"];
+			var field = arr["parameter"];
+			if ( isNaN(marks) || (marks < 0 || marks > 100)) {$("#" + field + "-msg").show("slow"); return;}
+		}
 		if(toggler.value=="DOB"){
-			val3=arr["value2"]
+			val3=arr["value2"];
+			// validating dob 
+			var entDOB = new Date(val3);
+			var yage = new Date("2015-01-01");
+			var old = new Date("1890-01-01");
+			if ( entDOB > yage ) {
+				$("#dob-msg").show("slow"); 
+				return;
+			} else if ( entDOB < old ) {
+				$("#dob-old-msg").show("slow");
+				return;
+			}
 		}
 		var val1=val1+"/"+val2+"/"+val3;
 		console.log(val1);
@@ -65,5 +90,4 @@ $(document).ready(function() {
 		};
 		
 	}
-
 });
