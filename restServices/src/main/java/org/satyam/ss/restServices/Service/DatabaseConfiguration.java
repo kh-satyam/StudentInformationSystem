65a014bf9c71c69479c7a383957c9df54666b5c8
Jagmeet Singh <jagmeet787@gmail.com>
2018-11-30 12:17:00 +0530

    some changes

--- restServices/src/main/java/org/satyam/ss/restServices/Service/DatabaseConfiguration.java
+++ restServices/src/main/java/org/satyam/ss/restServices/Service/DatabaseConfiguration.java
@@ -3,8 +3,8 @@ package org.satyam.ss.restServices.Service;
 public class DatabaseConfiguration {
 	public static String dataBaseUrl = "jdbc:mysql://localhost:3306/studentproject?useSSL=false";
 	public static String userName = "root";
-	public static String password = "password";
+	public static String password = "";
 	public static String dataBaseUrl2 = "jdbc:mysql://localhost:3306/studentproject?useSSL=false";
 	public static String userName2 = "root";
-	public static String password2 = "password";
+	public static String password2 = "";
 }
